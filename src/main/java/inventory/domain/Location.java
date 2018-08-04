0211dfc2387079baeaa636fa2ef7f66b6c31b609
Sekhar Ramaraju <sramaraju@in.synaptics.com>
2018-08-04 11:25:09 +0530

    Added location table
    added location table

---
+++ src/main/java/inventory/domain/Location.java
@@ -0,0 +1,96 @@
+package inventory.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Location.
+ */
+@Entity
+@Table(name = "location")
+public class Location implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 4, max = 50)
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Location status(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Location location = (Location) o;
+        if (location.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), location.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", status='" + getStatus() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/inventory/repository/LocationRepository.java
@@ -0,0 +1,15 @@
+package inventory.repository;
+
+import inventory.domain.Location;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Location entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long>, JpaSpecificationExecutor<Location> {
+
+}
